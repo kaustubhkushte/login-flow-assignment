8e73624f78f8bfc03e913eeb9ba4f17e5e6775d8
Kaustubh Kushte <kaustubhkushte@gmail.com>
2026-04-30 11:42:22 +0530

    Initial commit

---
+++ LoginFLow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginFLow
+//
+//  Created by Kaustubh Kushte on 30/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginFLow/LoginFLowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFLowApp.swift
+//  LoginFLow
+//
+//  Created by Kaustubh Kushte on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LoginFLowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
